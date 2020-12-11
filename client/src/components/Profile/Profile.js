9a0cbfdc32408c047512ad41fdfed626aa2fd8de
Kanoa McKay <troy4060@gmail.com>
2020-12-10 18:14:33 -0800

    Update to Profile.js
    Update on the profile.js, not done

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { Input } from '@material-ui/core';
+import {formControl} from '@material-ui/core';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -27,8 +34,19 @@ export default function Profile() {
 					<Grid item xs={12}>
 						<Paper className={classes.paper}>
 							<h2>Personal info</h2>
-
-							{/* CODE */}
+							<Grid item xs={12} sm={6}>
+          						<Paper className={classes.paper}>xs=12 sm=6</Paper>
+								{/* <Avatar alt="User Image" src="img location here" className={classes.large} /> */}
+								</Grid>
+							<Grid item xs={12} sm={6}>
+							<Paper classname={classes.paper}>xs=12 sm=6</Paper>
+							User info: First name, last name,Email, department,
+							</Grid>
+							<Grid item xs={12}>
+								<Paper className={classes.paper}>
+									user bio
+								</Paper>
+							</Grid>
 						</Paper>
 					</Grid>
 				</Grid>
