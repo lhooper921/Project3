8b34cd65ce6e9c7d566b76b7475bd2d4f5aac96e
Lauren Hooper <lluther921@gmail.com>
2020-12-12 11:36:45 -0800

    login styling

--- client/src/components/LogIn/LogIn.js
+++ client/src/components/LogIn/LogIn.js
@@ -16,23 +16,58 @@ import axios from 'axios';
 const useStyles = (theme) => ({
 	root: {
 		flexGrow: 1,
-		marginTop: '150px',
-		marginLeft: '200px',
-		marginRight: '200px'
+		marginLeft: '25px',
+		marginRight: '25px',
+		// backgroundColor: 'floralwhite',
+		padding: '25px'
 	},
 	paper: {
-		padding: theme.spacing(2),
+		padding: theme.spacing(3),
+		paddingBottom: theme.spacing(4),
+		paddingTop: theme.spacing(0),
 		textAlign: 'center',
 		color: theme.palette.text.secondary,
-		backgroundColor: 'lightgray'
-	},
+		backgroundColor: 'lightgray',
+		marginBottom: '25px'
 
+	},
+	paper2: {
+
+
+		textAlign: 'center',
+		color: theme.palette.text.secondary,
+		backgroundColor: '#F0F0F0',
+
+	},
+	paper3: {
+
+		textAlign: 'center',
+		color: theme.palette.text.secondary,
+		backgroundColor: '#F0F0F0',
+
+	},
 	texts: {
 		margin: 'auto',
 		width: '50%',
 		border: '3px solid teal',
 		padding: '30px'
+	},
+	input: {
+		backgroundColor: '#F0F0F0',
+
+	},
+	formControl: {
+		margin: theme.spacing(1),
+		minWidth: 120
+	},
+	selectEmpty: {
+		marginTop: theme.spacing(2)
+	},
+	button: {
+		marginLeft: '20px',
+		marginTop: '30px'
 	}
+		
 });
 
 class Login extends Component {
@@ -94,18 +129,25 @@ class Login extends Component {
 	render() {
 		const { classes } = this.props;
 		return (
-			<div>
+			<div class='container-fluid' style={{ minHeight: '1vh' }}>
 				<div className={classes.root}>
 					<Grid container spacing={3}>
-						<Grid item xs={12}>
+						
+						<Grid item 
+						  container
+						  direction="row"
+						  justify="center"
+						  alignItems="center"
+						 
+						>
 							<Paper className={classes.paper}>
 								<h2>Login</h2>
-								<Container className={classes.texts}>
-									<Col>
-										<Row>
-											<TextField
+								<Paper elevation={3} className={classes.paper3} style={{ paddingTop: '20px' }}>
+								<form className={classes.root} noValidate autoComplete="off">
+	<TextField
 												id="inputUser"
 												label="User"
+												style={{ width: '60%' }} 
 												InputProps={{
 													startAdornment: (
 														<InputAdornment position="start">
@@ -117,6 +159,35 @@ class Login extends Component {
 												value={this.state.user}
 												autoFocus
 											/>
+											<TextField
+											style={{ width: '60%' }} 
+												type="password"
+												id="inputPassword"
+												label="Password"
+												InputProps={{
+													startAdornment: (
+														<InputAdornment position="start">
+															<VpnKey />
+														</InputAdornment>
+													)
+												}}
+												onChange={this.changePassword}
+												value={this.state.password}
+											/>
+												<Button variant="primary" onClick={this.onSubmit}>
+												<Link to="/MainPage/Home"> Log In</Link>
+											</Button>
+											<Link to="/Register" className="navlink">
+												<Button variant="secondary">Register</Button>
+											</Link>
+
+								</form>
+</Paper>
+
+								{/* <Container className={classes.texts}>
+									<Col>
+										<Row>
+										
 										</Row>
 
 										<Row>
@@ -147,8 +218,8 @@ class Login extends Component {
 												<Button variant="secondary">Register</Button>
 											</Link>
 										</Row>
-									</Col>
-								</Container>
+									</Col> */}
+								{/* </Container> */}
 							</Paper>
 						</Grid>
 					</Grid>
